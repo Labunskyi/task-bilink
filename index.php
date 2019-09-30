05d24d729f4b21a4618b5abf47ad4b16c670f1c0
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-10-01 00:46:22 +0300

    fix displaying empty day payments

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 include_once 'libs/Payments.php';
 
-$payments_on_date = $payments->getPayments('2017-11-08');
+$payments_on_date = $payments->getPayments('2017-11-07');
 
 $create_payment = $payments->createPayment(200, 3);
 
-$create_report = $payments->createReport('2019-10-01');
+$create_report = $payments->createReport('2019-10-02');
 
 
 
--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,8 @@
  <?php
-if (empty($payments_on_date)) {
-} else {
+$x = (array)$payments_on_date;
+if (empty($x)) {
 	print_r($payments_on_date);
+} else {
+	echo 'No payments in such day';
 }
 ?>
